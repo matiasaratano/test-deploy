4eb5ac07f2b8d9e9a63cbde493eafd536f6d64ed
Matias <matiasaratano@gmail.com>
2024-07-21 16:29:32 +0200

    test

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,18 +1,16 @@
-import { Router } from "express";
-import UserController from '../controllers/UserController.js'
-import  AdminAuth  from '../middleware/AdminAuth.js';
+import { Router } from 'express';
+import UserController from '../controllers/UserController.js';
+import AdminAuth from '../middleware/AdminAuth.js';
 
-
-const userRoutes = Router()
-const userController = new UserController()
+const userRoutes = Router();
+const userController = new UserController();
 
 //CRUD
-userRoutes.post("", AdminAuth,userController.createUser)
-userRoutes.get("", userController.getAllUsers)
-userRoutes.get("/:id", userController.getUserById)
-userRoutes.put("/:id", AdminAuth,userController.updateUser)
-userRoutes.delete("/:id", AdminAuth, userController.deleteUser)
-userRoutes.post("/login", userController.login)
+userRoutes.post('', userController.createUser);
+userRoutes.get('', userController.getAllUsers);
+userRoutes.get('/:id', userController.getUserById);
+userRoutes.put('/:id', AdminAuth, userController.updateUser);
+userRoutes.delete('/:id', AdminAuth, userController.deleteUser);
+userRoutes.post('/login', userController.login);
 
-
-export default userRoutes
+export default userRoutes;
